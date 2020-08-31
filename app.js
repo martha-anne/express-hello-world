64748a77646166fdf0a90595818002647bd4a8d7
martha <miss.martha.anne@googlemail.com>
2020-08-31 17:21:49 +0100

    Change puppeteer version

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  console.log("building pdf");
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://www.payaca.com/", { waitUntil: "networkidle0" });
+  const pdf = await page.pdf({ format: "A4" });
+  await browser.close();
+  console.log(pdf);
+  console.log("pdf built");
   console.log(`Example app listening at http://localhost:${port}`);
 });
