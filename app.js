f39b66c0c1cd0f8dcca537feab241766dc4a3314
martha <miss.martha.anne@googlemail.com>
2020-09-01 08:35:26 +0100

    Switch to use playwright

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const playwright = require("playwright");
 const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
   console.log("building pdf");
 
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto("https://www.payaca.com/", { waitUntil: "networkidle0" });
+  const browser = await playwright["chromium"].launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://payaca.com/");
   const pdf = await page.pdf({ format: "A4" });
   await browser.close();
-  console.log(pdf);
-  console.log("pdf built");
   // return pdf
-  res.send("Hello World!");
+  res.send(pdf);
 });
 
 app.listen(port, async () => {
   console.log("building pdf");
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.payaca.com/", { waitUntil: "networkidle0" });
+  const browser = await playwright["chromium"].launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://payaca.com/");
   const pdf = await page.pdf({ format: "A4" });
   await browser.close();
-  console.log(pdf);
   console.log("pdf built");
   console.log(`Example app listening at http://localhost:${port}`);
 });
